f0f336ffafcb122ab4ce3e7142ea84ec00a5f531
elijahverdoorn <elijahverdoorn@gmail.com>
2016-09-24 20:39:55 -0500

    Finish KNN work, start adding datasets

--- knn/colleges.R
+++ knn/colleges.R
@@ -3,13 +3,20 @@ library(tidyr)
 library(dplyr)
 library(class)
 
-kval <- 3 # number of neighbors
+maxKVal <- 100
+means <- matrix(nrow = maxKVal, ncol = 1)
 
-colleges.df <- read.csv("collegeScoreboard2015Scaled2.csv") # read in the training data
-test.df <- read.csv("collegeScoreboard2015Scaled1.csv") # read in the testing data
+for (i in 1:maxKVal) {
+  kval <- i # number of neighbors
 
-response.predict <- knn(colleges.df[2:4], test[2:4], colleges.df[,c("Type")], k = kval) # build the KNN model
-response.predict # print the testing results
+  colleges.df <- read.csv("collegeScoreboard2015Scaled2.csv") # read in the training data
+  test.df <- read.csv("collegeScoreboard2015Scaled1.csv") # read in the testing data
 
-test.df <- mutate(test.df, class.pred = response.predict)
-mean(test.df["Type"] != test.df["class.pred"]) # how did we do? calculate the error rate
+  response.predict <- knn(colleges.df[2:4], test.df[2:4], colleges.df[,c("Type")], k = kval) # build the KNN model
+  response.predict # print the testing results
+
+  test.df <- mutate(test.df, class.pred = response.predict)
+  means[i] <- mean(test.df["Type"] != test.df["class.pred"]) # how did we do? calculate the error rate
+}
+
+min(means) # at k = 38
