a8c08dd98848812b87fc799ced136e0db5c1cabf
Elijah Verdoorn <elijahverdoorn@gmail.com>
2016-11-19 17:00:57 -0600

    Add the handwriting data for side project

---
+++ MNIST/mnist.py
@@ -0,0 +1,96 @@
+import os, struct
+from pylab import *
+from numpy import *
+import numpy as np
+from array import array as pyarray
+
+"""
+Loosely inspired by http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
+which is GPL licensed.
+"""
+
+def read(dataset = "training", path = "."):
+    """
+    Python function for importing the MNIST data set.  It returns an iterator
+    of 2-tuples with the first element being the label and the second element
+    being a numpy.uint8 2D array of pixel data for the given image.
+    """
+
+    if dataset is "training":
+        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+    elif dataset is "testing":
+        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+    else:
+        raise ValueError, "dataset must be 'testing' or 'training'"
+
+    # Load everything in some numpy arrays
+    with open(fname_lbl, 'rb') as flbl:
+        magic, num = struct.unpack(">II", flbl.read(8))
+        lbl = np.fromfile(flbl, dtype=np.int8)
+
+    with open(fname_img, 'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+        img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
+
+    get_img = lambda idx: (lbl[idx], img[idx])
+
+    # Create an iterator which returns each image in turn
+    for i in xrange(len(lbl)):
+        yield get_img(i)
+
+def show(image):
+    """
+    Render a given numpy.uint8 2D array of pixel data.
+    """
+    from matplotlib import pyplot
+    import matplotlib as mpl
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
+    imgplot.set_interpolation('nearest')
+    ax.xaxis.set_ticks_position('top')
+    ax.yaxis.set_ticks_position('left')
+    pyplot.show()
+
+def load_mnist(dataset="training", digits=np.arange(10), path="."):
+    """
+    Loads MNIST files into 3D numpy arrays
+
+    Adapted from: http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
+    """
+
+    if dataset == "training":
+        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+    elif dataset == "testing":
+        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+    else:
+        raise ValueError("dataset must be 'testing' or 'training'")
+
+    flbl = open(fname_lbl, 'rb')
+    magic_nr, size = struct.unpack(">II", flbl.read(8))
+    lbl = pyarray("b", flbl.read())
+    flbl.close()
+
+    fimg = open(fname_img, 'rb')
+    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    img = pyarray("B", fimg.read())
+    fimg.close()
+
+    ind = [ k for k in range(size) if lbl[k] in digits ]
+    N = len(ind)
+
+    images = zeros((N, rows, cols), dtype=uint8)
+    labels = zeros((N, 1), dtype=int8)
+    for i in range(len(ind)):
+        images[i] = array(img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
+        labels[i] = lbl[ind[i]]
+
+    return images, labels
+
+images, labels = load_mnist('training', digits=[2])
+imshow(images.mean(axis=0), cmap=cm.gray)
+show()
